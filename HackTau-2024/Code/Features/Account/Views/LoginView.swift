c8a18a8f50d72692ac239a69d04494cb3c8a3eec
Alex <korte@clemson.edu>
2024-03-02 15:04:38 -0500

    Add Google Sign In

--- HackTau-2024/Code/Features/Account/Views/LoginView.swift
+++ HackTau-2024/Code/Features/Account/Views/LoginView.swift
@@ -8,41 +8,27 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State private var username: String = ""
-    @State private var password: String = ""
+    @ObservedObject var viewModel = LoginViewModel()
 
     var body: some View {
         VStack {
-            Image("YourAppLogo") // Replace with your app's logo image
+            Image("your_app_logo") // Replace with your app logo
                 .resizable()
                 .scaledToFit()
-                .frame(width: 150, height: 150)
-
-            Text("Welcome Back")
-                .font(.largeTitle)
-.fontWeight(.semibold)
-
-            TextField("Username", text: $username)
                 .padding()
-                .background(Color.gray.opacity(0.15))
-                .cornerRadius(5)
 
-            SecureField("Password", text: $password)
-                .padding()
-                .background(Color.gray.opacity(0.15))
-                .cornerRadius(5)
-
-            Button("Login") {
-                // Handle login logic here (e.g., authenticate with a server)
+            Button("Sign in with Google") {
+                viewModel.googleSignIn()
             }
-            .foregroundColor(.white)
-            .frame(width: 200, height: 50)
-            .background(Color.blue)
-            .cornerRadius(10)
+            .buttonStyle(.borderedProminent)
 
-            Spacer() // Pushes content to the top
+            if let errorMessage = viewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+            }
         }
         .padding()
+        .navigationTitle("Login")
     }
 }
 
