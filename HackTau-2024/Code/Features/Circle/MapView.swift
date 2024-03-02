8d1b03b6d5cbd9e49ff70ac556f14afc63b5f03b
Mac Howe <mac@machowe.com>
2024-03-02 13:31:01 -0500

    Fixed mapView

--- HackTau-2024/Code/Features/Circle/MapView.swift
+++ HackTau-2024/Code/Features/Circle/MapView.swift
@@ -51,15 +51,3 @@ struct MapView_Previews: PreviewProvider {
         )))
     }
 }
-
-// SwiftUI Preview
-#if DEBUG
-struct MapView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapView(region: .constant(MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 34.0522, longitude: -118.2437),
-            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        )))
-    }
-}
-#endif
