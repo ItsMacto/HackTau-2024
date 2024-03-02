5b75083453f1ef912faaa7dcb13a5efb393422ef
Alex <korte@clemson.edu>
2024-03-02 13:24:27 -0500

    Add Login

---
+++ HackTau-2024/Code/Features/Account/Models/LoginModel.swift
@@ -0,0 +1,26 @@
+//
+//  LoginModel.swift
+//  HackTau-2024
+//
+//  Created by Alexander Korte on 3/2/24.
+//
+
+import Foundation
+
+class LoginViewModel: ObservableObject {
+//    @Published var isSignedIn = false // Example state
+//
+//    func signInWithGoogle() {
+//        FirebaseAPI.shared.signInWithGoogle { result in
+//            switch result {
+//            case .success(let user):
+//                // Handle successful sign-in (e.g., update isSignedIn)
+//                print("User signed in: \(user)")
+//                self.isSignedIn = true
+//            case .failure(let error):
+//                // Handle error
+//                print("Sign-in error: \(error)")
+//            }
+//        }
+//    }
+}
