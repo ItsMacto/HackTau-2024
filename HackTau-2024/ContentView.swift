a46649aee390961fd00d8f480346fb0bef0bbba9
Mac Howe <mac@machowe.com>
2024-03-02 11:54:49 -0500

    Initial Commit

---
+++ HackTau-2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackTau-2024
+//
+//  Created by Mac Howe  on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackTau-2024/HackTau_2024App.swift
@@ -0,0 +1,17 @@
+//
+//  HackTau_2024App.swift
+//  HackTau-2024
+//
+//  Created by Mac Howe  on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HackTau_2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
