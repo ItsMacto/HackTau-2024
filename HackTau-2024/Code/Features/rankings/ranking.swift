4f205228ded11cb20a3a14d7399b5a604dc457d5
Mac Howe <mac@machowe.com>
2024-03-03 03:55:09 -0500

    ranking

---
+++ HackTau-2024/Code/Features/rankings/ranking.swift
@@ -0,0 +1,18 @@
+//
+//  ranking.swift
+//  HackTau-2024
+//
+//  Created by Mac Howe  on 3/3/24.
+//
+
+import SwiftUI
+
+struct ranking: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ranking()
+}
